907afd4c324bae4a8107cca2987c6dac09b43c4e
Philipp Benz <phibenz@gmail.com>
2022-01-07 17:42:19 +0900

    a

--- experiment/fgsm/fgsm_baseline_eps8.sh
+++ experiment/fgsm/fgsm_baseline_eps8.sh
@@ -1,13 +1,26 @@
+# python RA_train_fgsm.py --batch-size 128 \
+#         --data-dir /dev/shm \
+#         --epochs 30 \
+#         --lr-schedule cyclic \
+#         --lr-min 0. \
+#         --lr-max 0.2 \
+#         --weight-decay 5e-4 \
+#         --epsilon 8 \
+#         --alpha 10 \
+#         --delta-init zero \
+#         --out-dir fgsm_baseline \
+#         --seed 0 \
+
+
 python RA_train_fgsm.py --batch-size 128 \
         --data-dir /dev/shm \
         --epochs 30 \
         --lr-schedule cyclic \
         --lr-min 0. \
-        --lr-max 0.2 \
+        --lr-max 0.3 \
         --weight-decay 5e-4 \
         --epsilon 8 \
         --alpha 10 \
-        --delta-init zero \
+        --delta-init random \
         --out-dir fgsm_baseline \
         --seed 0 \
-
