1d9c5565a7723063d1ea08066fdc53b25d925348
Philipp Benz <phibenz@gmail.com>
2022-01-19 17:57:15 +0900

    figure kl regurlization

--- FGSM_experiment/16eps/Autoattack_seed2.sh
+++ FGSM_experiment/16eps/Autoattack_seed2.sh
@@ -1,18 +1,21 @@
 python auto_attack_evaluation.py --epsilon 16 \
-        --testing_file "log_files/FGSM_NoiseAug_type_size-eps16-step_size20/FGSM_NoiseAug_type_size-eps16-step_size20-image_normalize--NoiseAug-_type_normal-noise_aug_size_3.0--epochs_30-lr_schedule_cyclic-lr_max_0.3-epsilon_16-attack_steps_7-alpha_20.0-delta_init_zero-zero_one_clamp_1-seed_2/20220112132809/model.pth"
+        --testing_file "log_files/remake_noise_align_normal_eps16_/size_2/remake_noise_align_normal_eps16_/size_2-image_normalize--NoiseAug-_type_normal-noise_aug_size_2.0--epochs_30-lr_schedule_cyclic-lr_max_0.3-epsilon_16-attack_steps_7-alpha_20.0-delta_init_zero-zero_one_clamp_1-seed_5/20220113135739/model.pth"
 
 python auto_attack_evaluation.py --epsilon 16 \
-        --testing_file "log_files/FGSM_NoiseAug_type_size-eps16-step_size20/FGSM_NoiseAug_type_size-eps16-step_size20-image_normalize--NoiseAug-_type_normal-noise_aug_size_2.0--epochs_30-lr_schedule_cyclic-lr_max_0.3-epsilon_16-attack_steps_7-alpha_20.0-delta_init_zero-zero_one_clamp_1-seed_2/20220112123453/model.pth"
+        --testing_file "log_files/remake_noise_align_normal_eps16_/size_1/remake_noise_align_normal_eps16_/size_1-image_normalize--NoiseAug-_type_normal-noise_aug_size_1.0--epochs_30-lr_schedule_cyclic-lr_max_0.3-epsilon_16-attack_steps_7-alpha_20.0-delta_init_zero-zero_one_clamp_1-seed_5/20220113125348/model.pth"
 
-python auto_attack_evaluation.py --epsilon 16 \
-        --testing_file "log_files/FGSM_NoiseAug_type_size-eps16-step_size20/FGSM_NoiseAug_type_size-eps16-step_size20-image_normalize--NoiseAug-_type_normal-noise_aug_size_1.0--epochs_30-lr_schedule_cyclic-lr_max_0.3-epsilon_16-attack_steps_7-alpha_20.0-delta_init_zero-zero_one_clamp_1-seed_2/20220112114145/model.pth"
+# python auto_attack_evaluation.py --epsilon 16 \
+#         --testing_file "log_files/FGSM_NoiseAug_type_size-eps16-step_size20/FGSM_NoiseAug_type_size-eps16-step_size20-image_normalize--NoiseAug-_type_normal-noise_aug_size_2.0--epochs_30-lr_schedule_cyclic-lr_max_0.3-epsilon_16-attack_steps_7-alpha_20.0-delta_init_zero-zero_one_clamp_1-seed_2/20220112123453/model.pth"
 
-python auto_attack_evaluation.py --epsilon 16 \
-        --testing_file "log_files/FGSM_NoiseAug_type_size-eps16-step_size20/FGSM_NoiseAug_type_size-eps16-step_size20-image_normalize--NoiseAug-_type_uniform-noise_aug_size_3.0--epochs_30-lr_schedule_cyclic-lr_max_0.3-epsilon_16-attack_steps_7-alpha_20.0-delta_init_zero-zero_one_clamp_1-seed_2/20220112104911/model.pth"
+# python auto_attack_evaluation.py --epsilon 16 \
+#         --testing_file "log_files/FGSM_NoiseAug_type_size-eps16-step_size20/FGSM_NoiseAug_type_size-eps16-step_size20-image_normalize--NoiseAug-_type_normal-noise_aug_size_1.0--epochs_30-lr_schedule_cyclic-lr_max_0.3-epsilon_16-attack_steps_7-alpha_20.0-delta_init_zero-zero_one_clamp_1-seed_2/20220112114145/model.pth"
 
-python auto_attack_evaluation.py --epsilon 16 \
-        --testing_file "log_files/FGSM_NoiseAug_type_size-eps16-step_size20/FGSM_NoiseAug_type_size-eps16-step_size20-image_normalize--NoiseAug-_type_uniform-noise_aug_size_2.0--epochs_30-lr_schedule_cyclic-lr_max_0.3-epsilon_16-attack_steps_7-alpha_20.0-delta_init_zero-zero_one_clamp_1-seed_2/20220112095620/model.pth"
+# python auto_attack_evaluation.py --epsilon 16 \
+#         --testing_file "log_files/FGSM_NoiseAug_type_size-eps16-step_size20/FGSM_NoiseAug_type_size-eps16-step_size20-image_normalize--NoiseAug-_type_uniform-noise_aug_size_3.0--epochs_30-lr_schedule_cyclic-lr_max_0.3-epsilon_16-attack_steps_7-alpha_20.0-delta_init_zero-zero_one_clamp_1-seed_2/20220112104911/model.pth"
 
-python auto_attack_evaluation.py --epsilon 16 \
-        --testing_file "log_files/FGSM_NoiseAug_type_size-eps16-step_size20/FGSM_NoiseAug_type_size-eps16-step_size20-image_normalize--NoiseAug-_type_uniform-noise_aug_size_1.0--epochs_30-lr_schedule_cyclic-lr_max_0.3-epsilon_16-attack_steps_7-alpha_20.0-delta_init_zero-zero_one_clamp_1-seed_2/20220112090353/model.pth"
+# python auto_attack_evaluation.py --epsilon 16 \
+#         --testing_file "log_files/FGSM_NoiseAug_type_size-eps16-step_size20/FGSM_NoiseAug_type_size-eps16-step_size20-image_normalize--NoiseAug-_type_uniform-noise_aug_size_2.0--epochs_30-lr_schedule_cyclic-lr_max_0.3-epsilon_16-attack_steps_7-alpha_20.0-delta_init_zero-zero_one_clamp_1-seed_2/20220112095620/model.pth"
+
+# python auto_attack_evaluation.py --epsilon 16 \
+#         --testing_file "log_files/FGSM_NoiseAug_type_size-eps16-step_size20/FGSM_NoiseAug_type_size-eps16-step_size20-image_normalize--NoiseAug-_type_uniform-noise_aug_size_1.0--epochs_30-lr_schedule_cyclic-lr_max_0.3-epsilon_16-attack_steps_7-alpha_20.0-delta_init_zero-zero_one_clamp_1-seed_2/20220112090353/model.pth"
 
